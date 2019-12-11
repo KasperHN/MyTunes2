cbb10d6be6a7c4463cdba1e56f06354e680a2173
KasperHN <kasper_130195@live.dk>
2019-12-11 10:52:02 +0100

    Update SongModel.java

--- MyTunes1/src/be/SongModel.java
+++ MyTunes1/src/be/SongModel.java
@@ -13,7 +13,11 @@
 
 package be;
 
+import java.io.IOException;
 import java.util.Scanner;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javazoom.jl.decoder.Manager;
 
 
 
@@ -74,26 +78,126 @@ private MusicPlayer song1=null, song2=null, song3=null, song4=null;
         }    
     }
     
-    public Song getSong(int songNumber) 
+       
+    private ObservableList<Song> allSongs = FXCollections.observableArrayList();
+
+    private final LogicFacade logiclayer;
+
+    /*
+    Initialises the constructor and the logic layer
+    */
+    public SongModel() throws IOException 
     {
-        if (songNumber == 1){
-            return song1;
+        logiclayer = (LogicFacade) new Manager();
+    }
+
+    /*
+    Gets all existing songs
+    */
+    public ObservableList<Song> getSongs() 
+    {
+        allSongs = FXCollections.observableArrayList();
+        allSongs.addAll(logiclayer.getAllSongs());
+        return allSongs;
+    }
+
+    private String title; // Title of song
+    private String artist; // Artist of song
+    private String category; // Category of song
+    private final int playtime; // Song playtime in seconds
+    private final String playtimeString; // Song playtime in minute:second format
+    private String location; // Song URL on the system
+    private final int ID; // Unique song ID in the database
+    private int locationInList; // Song ID thats hidden from the user. Its used mainly for moving the song up and down the list in database.
+    private int IDinsideList = 0; // Song ID thats visable to the user once displayed in the Playlist songs table
+
+    public SongModel(String title, String artist, String category, int playtime, String location, int ID) 
+    {
+        this.title = title;
+        this.artist = artist;
+        this.category = category;
+        this.playtime = playtime;
+        playtimeString = getPlaytimeString();
+        this.location = location;
+        this.ID = ID;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getIDinsideList() {
+        return IDinsideList;
+    }
+
+    public void setIDinsideList(int IDinsideList) {
+        this.IDinsideList = IDinsideList;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public int getLocationInList() {
+        return locationInList;
+    }
+
+    public void setLocationInList(int locationInList) {
+        this.locationInList = locationInList;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public int getPlaytime() {
+        return playtime;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+    public String getPlaytimeString() 
+    {
+        String minutesString;
+        String secondString;
+        int minutes = playtime / 60;
+        if (minutes < 10) {
+            minutesString = "0" + minutes;
+        } else {
+            minutesString = "" + minutes;
         }
-        else if (songNumber == 2)
-        {   
-            return song2;
+        int seconds = playtime % 60;
+        if (10 > seconds) {
+            secondString = "0" + seconds;
+        } else {
+            secondString = "" + seconds;
         }
-        else if (songNumber == 3)
-        {
-            return song3;
-        }
-        else if (songNumber == 4)
-        {
-            return song4;
-        }
-        else 
-        {
-            return song1; //This condition will never be met as the switch statement limits input to 1-4.
-        }
-    }   
+        return minutesString + ":" + secondString;
+    }
 }
